02382954b2462a943ce10ee81c7abb7cb1cd18e9
Vladimir Karpov <w-karpov@mail.ru>
2020-05-01 10:46:58 +0300

    Добавлены анализаторы кода для веб-сокет сервиса #17

--- websocket/index.js
+++ websocket/index.js
@@ -1,55 +1,59 @@
-let WebSocket = require('ws');
+const WebSocket = require('ws')
 
-let fs = require('fs');
-let jwt = require('jsonwebtoken');
-let dotenv = require('dotenv');
-let amqp = require('amqplib/callback_api');
+const fs = require('fs')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv')
+const amqp = require('amqplib/callback_api')
 
-dotenv.load();
+dotenv.load()
 
-let server = new WebSocket.Server({ port: 8000 });
-let jwtKey = fs.readFileSync(process.env.WS_JWT_PUBLIC_KEY);
+const server = new WebSocket.Server({ port: 8000 })
+const jwtKey = fs.readFileSync(process.env.WS_JWT_PUBLIC_KEY)
 
 server.on('connection', function (ws, request) {
-  console.log('connected:', request.connection.remoteAddress);
+  console.log('connected:', request.connection.remoteAddress)
 
   ws.on('message', function (message) {
-    let data = JSON.parse(message);
+    const data = JSON.parse(message)
     if (data.type && data.type === 'auth') {
       try {
-        let token = jwt.verify(data.token, jwtKey, {algorithms: ['RS256']});
-        ws.user_id = token.sub;
+        const token = jwt.verify(data.token, jwtKey, { algorithms: ['RS256'] })
+        ws.user_id = token.sub
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     }
-  });
-});
+  })
+})
 
-amqp.connect(process.env.WS_AMQP_URI, function(err, conn) {
+amqp.connect(process.env.WS_AMQP_URI, function (err, conn) {
   if (err) {
-    console.log(err);
-    return;
+    console.log(err)
+    return
   }
-  conn.createChannel(function(err, ch) {
-    let queue = 'tasks.notifications.queue';
-    ch.consume(queue, function(message) {
-      let value = JSON.parse(message.content);
+  conn.createChannel(function (connErr, ch) {
+    const queue = 'tasks.notifications.queue'
+    ch.consume(
+      queue,
+      function (message) {
+        const value = JSON.parse(message.content)
 
-      server.clients.forEach(ws => {
-        if (ws.user_id) {
-          switch (value.visibility) {
-            case 'private':
-              if (ws.user_id === value.author_id) {
-                ws.send(message.content.toString());
-              }
-              break;
-            case 'public':
-              ws.send(message.content.toString());
-              break;
+        server.clients.forEach((ws) => {
+          if (ws.user_id) {
+            switch (value.visibility) {
+              case 'private':
+                if (ws.user_id === value.author_id) {
+                  ws.send(message.content.toString())
+                }
+                break
+              case 'public':
+                ws.send(message.content.toString())
+                break
+            }
           }
-        }
-      })
-    }, {noAck: true});
-  });
-});
+        })
+      },
+      { noAck: true }
+    )
+  })
+})
